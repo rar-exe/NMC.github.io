c2284ec28734e7fe7e6dfad7626afc4c8ccf8274
Никита <129552667+rar-exe@users.noreply.github.com>
2024-02-17 15:01:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    const elements = document.querySelectorAll('[data-mask="phone"]') // ищем все поля с атрибутом data-mask="phone"
-    if (!elements) return // если таких нет, прекращаем выполнение функции
-    const phoneOptions = { // создаем объект параметров
-      mask: '+0(000)000-00-00',// задаем единственный параметр mask
+  const elements = document.querySelectorAll('[data-mask="phone"]')
+  if (!elements) return
+    const phoneOptions = {
+      mask: '+0(000)000-00-00',
     }
-    elements.forEach(el => { // для каждого найденного поля с атрибутом [data-mask="phone"]
-      IMask(el, phoneOptions) // инициализируем плагин с установленными выше параметрами
-    })
-  
+  elements.forEach(el => {
+    IMask(el, phoneOptions)
   })
+})
+
+let adress = document.querySelectorAll(".adress")
+
+adress.forEach(function(adr){
+  adr.addEventListener("click", function(){
+    navigator.clipboard.writeText(adr.innerHTML);
+  })
+});
