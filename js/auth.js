a58f585de4abfdf2affe3b5d9b373ceb0c3c9bb9
Никита <fedor1voina@gmail.com>
2024-03-05 01:15:27 +0300

    auth and services

---
+++ js/auth.js
@@ -0,0 +1,29 @@
+let flag = false;
+
+$('.registration').on('click', function(){
+    $('.auth').toggleClass('flip');
+    flag = true;
+});
+
+$('.return_btn').on('click', function(){
+    $('.auth').toggleClass('flip');
+    flag = false;
+});
+
+let btn = document.querySelector('.login');
+let close_btns = document.querySelectorAll(".close_btn")
+var bg = document.querySelector('.bg');
+
+function showBlock() {
+    bg.classList.add('b-show');
+}
+
+function hideBlock() {
+    bg.classList.remove('b-show');
+    if (flag) {$('.auth').toggleClass('flip'); }
+}
+
+close_btns.forEach(function(close_btn){
+    close_btn.addEventListener("click", hideBlock)
+})
+btn.addEventListener('click', showBlock);
